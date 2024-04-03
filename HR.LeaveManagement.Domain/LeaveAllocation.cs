1c10f1d2ed7f70a3dcdcdd44fefc4537c17f49a8
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-03 15:03:17 +0200

    Feat : Added LeaveAllocation class

---
+++ HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveAllocation
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }   
+
+}
