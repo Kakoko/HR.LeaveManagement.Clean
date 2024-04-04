4a42ded5110125ea671008f7a0510a457ee073f4
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-04 09:34:32 +0200

    Feat : Addedd Generic Repository Pattern

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence
+{
+    public interface IGenericRepository<T> where T : class 
+    {
+       //Implemented async methods
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetAsync();
+        Task<T> CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+            
+       
+    }
+
+}
