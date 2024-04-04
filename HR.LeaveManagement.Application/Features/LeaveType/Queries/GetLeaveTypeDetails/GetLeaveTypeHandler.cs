3bb28b922fa0d0180630e2f41d70516562375c5a
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-04 10:39:58 +0200

    Feat : Added Query Classes

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails
+{
+    public class GetLeaveTypeHandler : IRequestHandler<GetLeaveTypeDetailsQuery, LeaveTypeDetailsDto>
+    {
+
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        public GetLeaveTypeHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        public async Task<LeaveTypeDetailsDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var leaveTypes = await _leaveTypeRepository.GetByIdAsync(request.id);
+
+            return _mapper.Map<LeaveTypeDetailsDto>(leaveTypes);
+        }
+
+    }
+}
