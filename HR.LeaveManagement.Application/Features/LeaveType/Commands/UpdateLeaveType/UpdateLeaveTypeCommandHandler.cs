344edd7fa5a64175536006aa71cfef9024f0df1f
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-04 10:55:30 +0200

    Refactor : Implemented Logic in classes

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,29 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
 {
-    public class UpdateLeaveTypeCommandHandler
+    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
     {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            //Validate Incoming Data
+
+            //Convert to Domain Entity Type
+            var leaveTypeToUpdate = _mapper.Map<Domain.LeaveType>(request);
+
+            //add to DB
+            await _leaveTypeRepository.UpdateAsync(leaveTypeToUpdate);
+           
+            //Return Unit Value
+            return Unit.Value;
+        }
     }
 }
