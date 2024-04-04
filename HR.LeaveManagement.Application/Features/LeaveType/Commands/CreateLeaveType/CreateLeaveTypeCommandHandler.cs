8f6b87602df502e964ea849ce0cc46b4e57309da
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-04 10:49:46 +0200

    Added Update Operation Handlers

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+           //Validate Incoming Data
+
+
+            // Convert to Domain Entity Type
+            var leaveType = _mapper.Map<Domain.LeaveType>(request);
+
+            //Add to DB
+            await _leaveTypeRepository.CreateAsync(leaveType);
+
+            //return record id
+            return leaveType.Id;
+        }
+    }
+}
