aba7ef9c0f9aa76a2d10ffcfbd8e0be96fbbc3c4
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-04 11:02:43 +0200

    Feat : Implemented the Delete Command Class Logic

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,30 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType
 {
-    internal class DeleteLeaveTypeCommandHandler
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
     {
+
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository) => _leaveTypeRepository = leaveTypeRepository;
+       
+
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            // retrieve domain 
+            var leaveTypeToDelete = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+            // Verify records exists
+            if (leaveTypeToDelete == null)
+            {
+                throw new ApplicationException($"Leave Type with Id = {request.Id} not found");
+            }
+
+            //Delete from DB
+            await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
+
+            //return Unit Value
+            return Unit.Value;
+        }
     }
 }
