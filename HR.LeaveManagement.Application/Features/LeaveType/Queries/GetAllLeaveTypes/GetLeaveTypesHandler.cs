524dd4b4a56245c22685e0819dde839f90d32fbf
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-04 10:23:41 +0200

    Feat : Implemented GetLeaveTypesHandler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    public class GetLeaveTypesHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public GetLeaveTypesHandler(IMapper mapper , ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+        {
+            //Query DB
+            var leaveTypes = await _leaveTypeRepository.GetAsync();
+
+            // Convert Entity to DTO
+            var leaveTypesDto = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+            //Return Dto
+            return leaveTypesDto;
+        }
+    }
+}
