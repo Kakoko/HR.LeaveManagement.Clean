18018652ea6d83185b56c5b1a58666ac3240f5bb
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-18 11:46:23 +0200

    Feat : Added Contracts to ILeaveRequestRepository

--- HR.LeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -4,7 +4,9 @@ namespace HR.LeaveManagement.Application.Contracts.Persistence
 {
     public interface ILeaveRequestRepository : IGenericRepository<LeaveRequests>
     {
-
+        Task<LeaveRequests> GetLeaveRequestsWithDetails(int id);
+        Task<List<LeaveRequests>> GetLeaveRequestsWithDetails();
+        Task<List<LeaveRequests>> GetLeaveRequestsWithDetails(string userId);
     }   
 
 }
