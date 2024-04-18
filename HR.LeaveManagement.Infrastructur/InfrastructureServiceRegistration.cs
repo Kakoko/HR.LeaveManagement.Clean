fa63a21bb55c8b6b682a3335fbf6f6bfbf63785f
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-18 14:06:49 +0200

    feat : created Infrastructure Project

---
+++ HR.LeaveManagement.Infrastructur/InfrastructureServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Infrastructur
+{
+  
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+          
+            return services;
+        }
+    }
+}
