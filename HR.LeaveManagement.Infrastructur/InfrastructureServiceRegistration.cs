270cbff53159058aa78fb7b8776a62a4928343d0
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-18 14:58:19 +0200

    Feat : Added Logger to DI container

--- HR.LeaveManagement.Infrastructur/InfrastructureServiceRegistration.cs
+++ HR.LeaveManagement.Infrastructur/InfrastructureServiceRegistration.cs
@@ -1,6 +1,8 @@
 ﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Models;
 using HR.LeaveManagement.Infrastructur.EmailService;
+using HR.LeaveManagement.Infrastructur.LoggingService;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -16,6 +18,7 @@ namespace HR.LeaveManagement.Infrastructur
 
             services.Configure<EmailSettings>(configuration.GetSection("EmailSettings").Bind);
             services.AddTransient<IEmailSender, EmailSender>();
+            services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
             return services;
         }
     }
