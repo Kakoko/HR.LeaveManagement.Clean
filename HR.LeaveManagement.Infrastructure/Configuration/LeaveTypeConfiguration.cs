2f20234e4cb0f48b98c567f96ec8d08759937107
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-18 11:02:47 +0200

    Feat : added Configurations for seed data

--- HR.LeaveManagement.Infrastructure/Configuration/LeaveTypeConfiguration.cs
+++ HR.LeaveManagement.Infrastructure/Configuration/LeaveTypeConfiguration.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Persistence.Configuration
 {
-    public class LeaveTypeConfiguration
+    public class LeaveTypeConfiguration : IEntityTypeConfiguration<LeaveType>
     {
+        public void Configure(EntityTypeBuilder<LeaveType> builder)
+        {
+            builder.HasData(
+                new LeaveType 
+                { 
+                    Id = 1, 
+                    Name = "Vacation", 
+                    DefaultDays = 10, 
+                    DateModified = DateTime.Now, 
+                    DateCreated = DateTime.Now
+                });
+        }
     }
 }
