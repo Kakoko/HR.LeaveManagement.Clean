bf6c12edf52e0af6720a801b5cc9752e61fe5c7e
Simuzeche Kaluwa <simuzechek@gmail.com>
2024-04-18 11:30:16 +0200

    Feat : Implemented LeaveTypeRepository

--- HR.LeaveManagement.Infrastructure/Repositories/LeaveTypeRepository.cs
+++ HR.LeaveManagement.Infrastructure/Repositories/LeaveTypeRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,15 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Persistence.Repositories
 {
-    public class LeaveTypeRepository
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
     {
+        public LeaveTypeRepository(HRDatabaseContext context) : base(context)
+        {
+        }
+
+        public async Task<bool> IsLeaveTypeUnique(string name)
+        {
+            return await _context.LeaveTypes.AnyAsync(u => u.Name == name);
+        }
     }
 }
